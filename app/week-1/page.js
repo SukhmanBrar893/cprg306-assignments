18a51fdbaec2ab3eded74520bac9ae8db3b8b3e1
rudrapratap11 <rudrapratap@edu.sait.ca>
2025-02-04 10:42:03 -0700

    test

--- app/week-1/page.js
+++ app/week-1/page.js
@@ -5,7 +5,7 @@ export default function Page() {
         <p><Link className="font-bold bg-blue-200 p-0 m-1" href="../">Home</Link></p>
         <h1 className=" font-bold text-blue-900">Week 1 </h1>
         <p className="text-xl italic">
-          Congratulations on correctly setting up your development environment!!
+          HI Congratulations on correctly setting up your development environment!!
         </p>
       </main>
     );
